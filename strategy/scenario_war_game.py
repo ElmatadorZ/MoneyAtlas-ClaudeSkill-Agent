733041dd4fce45251b131bfa628095c040be4f12
Money Atlas <judgment007@gmail.com>
2026-03-19 13:08:41 +0700

    Create scenario_war_game.py

---
+++ strategy/scenario_war_game.py
@@ -0,0 +1,16 @@
+# strategy/scenario_war_game.py
+
+class ScenarioWarGame:
+
+    def simulate(self, macro, geo):
+
+        return [
+            {
+                "scenario": "Global Risk-Off",
+                "result": "equities down, gold up"
+            },
+            {
+                "scenario": "Liquidity Injection",
+                "result": "crypto & risk assets rally"
+            }
+        ]
